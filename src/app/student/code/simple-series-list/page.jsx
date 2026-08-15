37fe66c23c1faab005ce08e736f0d1a974c5017d
Jahid Hasan Rimel <zmzahidhasan181@gmail.com>
2026-08-15 16:57:06 +0600

    up

--- src/app/student/code/simple-series-list/page.jsx
+++ src/app/student/code/simple-series-list/page.jsx
@@ -1,5 +1,11 @@
 import SeriesListPage from "../_components/SeriesListPage";
 
 export default function SimpleSeriesListPage() {
-  return <SeriesListPage type="simple_series" title="Simple Series List" emptyText="No simple series is available." />;
+  return (
+    <SeriesListPage
+      type="simple_series"
+      title="Simple Series List"
+      emptyText="No simple series is available."
+    />
+  );
 }
